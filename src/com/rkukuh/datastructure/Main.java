176008c21233741a249772cf034ce9e580d89c37
R. Kukuh <rkukuh@gmail.com>
2018-07-20 11:23:01 +0700

    Save section 02: Array

---
+++ src/com/rkukuh/datastructure/Main.java
@@ -0,0 +1,10 @@
+package com.rkukuh.datastructure;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to modern Data Structure");
+
+    }
+}
