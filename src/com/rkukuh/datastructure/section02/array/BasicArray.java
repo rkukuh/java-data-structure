1f027db644ad10ae7ce9308f2c3c7cda3b3662f5
R. Kukuh <rkukuh@gmail.com>
2018-07-26 11:05:59 +0700

    Rename file

--- src/com/rkukuh/datastructure/section02/array/BasicArray.java
+++ src/com/rkukuh/datastructure/section02/array/BasicArray.java
@@ -1,6 +1,6 @@
 package com.rkukuh.datastructure.section02.array;
 
-public class Main {
+public class BasicArray {
 
     public static void main(String[] args)
     {
@@ -10,12 +10,14 @@ public class Main {
         numbers[1] = 35;
         numbers[2] = -15;
         numbers[3] = 7;
-        numbers[4] =55;
+        numbers[4] = 55;
         numbers[5] = 1;
         numbers[6] = -22;
 
         for (int i = 0; i < numbers.length; i++) {
+
             System.out.println(numbers[i]);
+
         }
     }
 }
