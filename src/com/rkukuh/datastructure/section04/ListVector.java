6e7a864c7de56ba0fbcb7b80e68320c2c5076759
R. Kukuh <rkukuh@gmail.com>
2018-07-29 00:04:22 +0700

    Add lesson: List using Vector

---
+++ src/com/rkukuh/datastructure/section04/ListVector.java
@@ -0,0 +1,20 @@
+package com.rkukuh.datastructure.section04;
+
+import java.util.List;
+import java.util.Vector;
+
+class ListVector
+{
+    public static void main(String[] args)
+    {
+        List<Employee> employees = new Vector<>();
+
+        employees.add(new Employee("Iron", "Man", 123));
+        employees.add(new Employee("Captain", "America", 456));
+        employees.add(new Employee("Black", "Panther", 789));
+        employees.add(new Employee("Thor", "Thunder", 321));
+
+        employees.forEach(System.out::println);
+    }
+
+}
