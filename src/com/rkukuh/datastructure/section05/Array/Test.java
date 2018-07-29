682bdc987d3945443e0859bb89e6f6362b01d276
R. Kukuh <rkukuh@gmail.com>
2018-07-29 13:46:58 +0700

    Add lesson: Stack using Array

---
+++ src/com/rkukuh/datastructure/section05/Array/Test.java
@@ -0,0 +1,27 @@
+package com.rkukuh.datastructure.section05.Array;
+
+import com.rkukuh.datastructure.Employee;
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        StackArray employees = new StackArray(10);
+
+        employees.push(new Employee("Iron", "Man", 123));
+        employees.push(new Employee("Captain", "America", 456));
+        employees.push(new Employee("Black", "Panther", 789));
+        employees.push(new Employee("Thor", "Thunder", 321));
+
+        employees.printStack();
+
+        System.out.println();
+        System.out.println("Peek: " + employees.peek());
+
+        System.out.println();
+        System.out.println("Popped: " + employees.pop());
+
+        System.out.println();
+        employees.printStack();
+    }
+}
