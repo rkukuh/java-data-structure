19755516cf8ace01f55718c6d396c724e00c31a2
R. Kukuh <rkukuh@gmail.com>
2018-07-29 23:08:57 +0700

    Add lesson: Linear Search

---
+++ src/com/rkukuh/DataStructure/ch08_Search/LinearSearch.java
@@ -0,0 +1,24 @@
+package com.rkukuh.DataStructure.ch08_Search;
+
+class LinearSearch
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = {20, 35, -15, 7, 55, 1, -22};
+
+        System.out.println(linearSearch(numbers, -15));
+        System.out.println(linearSearch(numbers, 1));
+        System.out.println(linearSearch(numbers, 8888));
+        System.out.println(linearSearch(numbers, -22));
+    }
+
+    private static int linearSearch(int[] input, int value)
+    {
+        for (int i = 0; i < input.length; i++) {
+
+            if (input[i] == value) return i;
+        }
+
+        return -1;
+    }
+}
