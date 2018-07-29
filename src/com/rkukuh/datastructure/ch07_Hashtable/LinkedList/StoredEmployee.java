b74888600b9ba235daf8a30fc588d5c3b742134c
R. Kukuh <rkukuh@gmail.com>
2018-07-29 19:20:07 +0700

    Add lesson: Hashtable using LinkedList

---
+++ src/com/rkukuh/datastructure/ch07_Hashtable/LinkedList/StoredEmployee.java
@@ -0,0 +1,15 @@
+package com.rkukuh.datastructure.ch07_Hashtable.LinkedList;
+
+import com.rkukuh.datastructure.Employee;
+
+class StoredEmployee
+{
+    public final String key;
+    public final Employee employee;
+
+    public StoredEmployee(String key, Employee employee)
+    {
+        this.key = key;
+        this.employee = employee;
+    }
+}
---
+++ src/com/rkukuh/datastructure/ch07_Hashtable/LinkedList/Test.java
@@ -0,0 +1,35 @@
+package com.rkukuh.datastructure.ch07_Hashtable.LinkedList;
+
+import com.rkukuh.datastructure.Employee;
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        HashtableLinkedList employees = new HashtableLinkedList();
+
+        Employee ironMan        = new Employee("Iron", "Man", 123);
+        Employee captAmerica    = new Employee("Captain", "America", 456);
+        Employee blackPanther   = new Employee("Black", "Panther", 789);
+        Employee thorThunder    = new Employee("Thor", "Thunder", 321);
+        Employee lokiBrother    = new Employee("Loki", "Brother", 321);
+
+        employees.put("iron", ironMan);
+        employees.put("captain", captAmerica);
+        employees.put("kingtchala", blackPanther);
+        employees.put("god", thorThunder);
+        employees.put("loki", lokiBrother);
+
+        employees.printHashtable();
+
+        System.out.println();
+        System.out.println("Retrieve 'captain' record: " + employees.get("captain"));
+
+        System.out.println();
+        System.out.println("Removed: " + employees.remove("iron"));
+        System.out.println("Removed: " + employees.remove("god"));
+
+        System.out.println();
+        employees.printHashtable();
+    }
+}
