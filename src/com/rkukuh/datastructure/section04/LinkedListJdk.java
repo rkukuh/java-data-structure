c1bbfc82f1946fc228dd6c81995e7d9fbf08d60e
R. Kukuh <rkukuh@gmail.com>
2018-07-29 07:57:46 +0700

    Add lesson: LinkedList using JDK

---
+++ src/com/rkukuh/datastructure/section04/LinkedListJdk.java
@@ -0,0 +1,66 @@
+package com.rkukuh.datastructure.section04;
+
+import com.rkukuh.datastructure.Employee;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListJdk
+{
+    public static void main(String[] args)
+    {
+        LinkedList<Employee> employees = new LinkedList<>();
+
+        Employee ironMan        = new Employee("Iron", "Man", 123);
+        Employee captAmerica    = new Employee("Captain", "America", 456);
+        Employee blackPanther   = new Employee("Black", "Panther", 789);
+        Employee thorThunder    = new Employee("Thor", "Thunder", 321);
+        Employee lokiBrother    = new Employee("Loki", "Brother", 654);
+
+        employees.addFirst(ironMan);
+        employees.addFirst(captAmerica);
+        employees.addFirst(blackPanther);
+        employees.addFirst(thorThunder);
+
+        Iterator iterator = employees.iterator();
+
+        System.out.print("HEAD -> ");
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next());
+            System.out.print(" <-> ");
+        }
+        System.out.println("null");
+
+        employees.addLast(lokiBrother);
+
+        iterator = employees.iterator();
+        System.out.print("HEAD -> ");
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next());
+            System.out.print(" <-> ");
+        }
+        System.out.println("null");
+
+        employees.removeFirst();
+        iterator = employees.iterator();
+        System.out.print("HEAD -> ");
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next());
+            System.out.print(" <-> ");
+        }
+        System.out.println("null");
+
+        employees.removeLast();
+        iterator = employees.iterator();
+        System.out.print("HEAD -> ");
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next());
+            System.out.print(" <-> ");
+        }
+        System.out.println("null");
+
+        System.out.println();
+        employees.forEach(System.out::println);
+    }
+
+}
